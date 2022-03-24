78a156a9b00f1dbb358d7d68434e6556d6572d07
Kael <kaelwd@gmail.com>
2022-03-25 00:06:49 +1100

    feat: close issues mentioned in pushed commits

--- close.js
+++ close.js
@@ -1,12 +1,37 @@
 const github = require('@actions/github')
 const core = require('@actions/core')
-const fs = require('fs').promises
 
 const context = github.context
 
 const token = core.getInput('token')
 const octokit = github.getOctokit(token)
 
+const closeRegexp = /(?:(?:close|resolve)[ds]?|fix(?:e[ds])?) #(\d+)/gi
+
 ;(async function run() {
-  //
+  if (context.eventName === 'push') {
+    const issues = Array.from(new Set(
+      (await Promise.all(
+        context.payload.commits.map(async commit => {
+          if (!commit.distinct) return
+
+          return [...commit.message.matchAll(closeRegexp)].map(match => +match[1])
+        })
+      )).flat().filter(v => v != null)
+    ))
+
+    if (!issues.length) return
+
+    core.notice(`Closing issue${issues.length > 1 ? 's' : ''} ${issues.map(v => `#${v}`).join(', ')}`)
+
+    await Promise.all(
+      issues.map(async issueNumber => {
+        return octokit.rest.issues.update({
+          ...context.repo,
+          issue_number: issueNumber,
+          state: 'closed',
+        })
+      })
+    )
+  }
 })()
--- dist/index.js
+++ dist/index.js
@@ -8470,15 +8470,40 @@ var __webpack_exports__ = {};
 (() => {
 const github = __nccwpck_require__(5911)
 const core = __nccwpck_require__(7962)
-const fs = __nccwpck_require__(5747).promises
 
 const context = github.context
 
 const token = core.getInput('token')
 const octokit = github.getOctokit(token)
 
+const closeRegexp = /(?:(?:close|resolve)[ds]?|fix(?:e[ds])?) #(\d+)/gi
+
 ;(async function run() {
-  //
+  if (context.eventName === 'push') {
+    const issues = Array.from(new Set(
+      (await Promise.all(
+        context.payload.commits.map(async commit => {
+          if (!commit.distinct) return
+
+          return [...commit.message.matchAll(closeRegexp)].map(match => +match[1])
+        })
+      )).flat().filter(v => v != null)
+    ))
+
+    if (!issues.length) return
+
+    core.notice(`Closing issue${issues.length > 1 ? 's' : ''} ${issues.map(v => `#${v}`).join(', ')}`)
+
+    await Promise.all(
+      issues.map(async issueNumber => {
+        return octokit.rest.issues.update({
+          ...context.repo,
+          issue_number: issueNumber,
+          state: 'closed',
+        })
+      })
+    )
+  }
 })()
 
 })();
